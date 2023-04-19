b0998aaa18a43eb77c0c5efd8e8fd985e8eb728b
Leo B Jacob <leobjacob@gmail.com>
2023-04-19 10:28:30 -0400

    Update URL

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000/api/v1';
+export const API_URL = 'https://railssms.herokuapp.com/api/v1';
 export const serverError = 'OOPS an error occured, please try after sometime';
